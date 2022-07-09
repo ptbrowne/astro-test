b3651e434b42713487a3606f810e009d7b8396e7
Patrick Browne <pt.browne@gmail.com>
2022-07-09 12:53:23 +0200

    feat: Statically build the data in

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -24,14 +24,8 @@ const usePromise = <T extends unknown>(promiser: () => Promise<T>) => {
   return data;
 };
 
-const parseRdf = (rows: Record<string, { value: string }>[]) => {
-  return rows.map((r) => mapValues(r, (v) => v.value));
-};
-
-export default ({ operatorIds }) => {
-  const data = usePromise(() =>
-    fetchData(operatorIds).then((x) => parseRdf(x.results.bindings))
-  );
+export default ({ operatorIds, data }) => {
+  console.log("data", { data });
   const names = {};
   const pivotted = useMemo(() => {
     const sorted = sortBy(data, (x) => parseInt(x.d_period, 10));
@@ -42,44 +36,42 @@ export default ({ operatorIds }) => {
         names[row.d_operator] = row.d_operator_name;
         res[row.d_operator] = row.d_total;
       }
-      console.log({ res });
       return res;
     });
   });
 
   return data ? (
     <div style={{ height: 400, width: 800 }}>
-      <ResponsiveContainer width="100%" height="100%">
-        <LineChart
-          width={500}
-          height={300}
-          data={pivotted}
-          margin={{
-            top: 5,
-            right: 30,
-            left: 20,
-            bottom: 5,
-          }}
-        >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="period" />
-          <YAxis />
-          <Tooltip />
-          <Legend />
-          {operatorIds.map((operatorId) => (
-            <Line
-              type="monotone"
-              dataKey={`https://energy.ld.admin.ch/elcom/electricityprice/operator/${operatorId}`}
-              name={
-                names[
-                  `https://energy.ld.admin.ch/elcom/electricityprice/operator/${operatorId}`
-                ]
-              }
-              activeDot={{ r: 8 }}
-            />
-          ))}
-        </LineChart>
-      </ResponsiveContainer>
+      <LineChart
+        width={500}
+        height={300}
+        data={pivotted}
+        margin={{
+          top: 5,
+          right: 30,
+          left: 20,
+          bottom: 5,
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="period" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        {operatorIds.map((operatorId) => (
+          <Line
+            key={operatorId}
+            type="monotone"
+            dataKey={`https://energy.ld.admin.ch/elcom/electricityprice/operator/${operatorId}`}
+            name={
+              names[
+                `https://energy.ld.admin.ch/elcom/electricityprice/operator/${operatorId}`
+              ]
+            }
+            activeDot={{ r: 8 }}
+          />
+        ))}
+      </LineChart>
     </div>
   ) : (
     <div>Loading data...</div>
--- src/data.ts
+++ src/data.ts
@@ -1,37 +1,30 @@
-const makeOperatorFilter = (operatorIds: string[]) => {
-  return operatorIds
-    .map(
-      (oid) =>
-        `?d_operator = <https://energy.ld.admin.ch/elcom/electricityprice/operator/${oid}>`
-    )
-    .join(" || ");
+const mapValues = (obj, fn) => {
+  const res = {};
+  for (let [k, v] of Object.entries(obj)) {
+    res[k] = fn(v, k);
+  }
+  return res;
 };
 
 const query = (
   operatorIds: string[]
-) => `SELECT DISTINCT ?d_period (AVG(?d_total) as ?d_total) ?d_operator_name ?d_operator
+) => `SELECT ?d_period (AVG(?d_total) as ?d_total) ?d_operator_name ?d_operator
 WHERE {
   <https://energy.ld.admin.ch/elcom/electricityprice> <https://cube.link/observationSet> ?observationSet0 .
   ?observationSet0 <https://cube.link/observation> ?source0 .
-  ?source0 <https://energy.ld.admin.ch/elcom/electricityprice/dimension/category> ?d_category .
-  ?source0 <https://energy.ld.admin.ch/elcom/electricityprice/dimension/municipality> ?d_municipality .
   ?source0 <https://energy.ld.admin.ch/elcom/electricityprice/dimension/operator> ?d_operator .
   ?source0 <https://energy.ld.admin.ch/elcom/electricityprice/dimension/period> ?d_period .
   ?source0 <https://energy.ld.admin.ch/elcom/electricityprice/dimension/total> ?d_total .
-  ?source0 <https://energy.ld.admin.ch/elcom/electricityprice/dimension/product> ?d_product .
   
-  FILTER (
-    ${makeOperatorFilter(operatorIds)}
-  )
-
-    OPTIONAL {
-    ?d_municipality <http://schema.org/containedInPlace> ?d_canton .
-        ?d_canton <http://schema.org/identifier> ?d_canton_id .
-
-    FILTER (
-      (?d_canton_id != "CHE")
-    )
+  VALUES ?d_operator {
+    ${operatorIds
+      .map(
+        (oid) =>
+          `<https://energy.ld.admin.ch/elcom/electricityprice/operator/${oid}>`
+      )
+      .join("\n")}
   }
+
   OPTIONAL {
     ?d_operator <http://schema.org/name> ?d_operator_en .
     FILTER (
@@ -49,10 +42,14 @@ WHERE {
 } 
 
 GROUP BY ?d_period ?d_category_name ?d_operator_name ?d_operator
-ORDER BY ?d_period
+ORDER BY ?d_period 
 `;
+
+const parseRdf = (rows: Record<string, { value: string }>[]) => {
+  return rows.map((r) => mapValues(r, (v) => v.value));
+};
+
 export const fetchLindas = (query: string) => {
-  console.log({ query });
   const resp = fetch("https://lindas.admin.ch/query", {
     body: `query=${encodeURIComponent(query)}`,
     method: "POST",
@@ -60,7 +57,9 @@ export const fetchLindas = (query: string) => {
       Accept: "application/sparql-results+json",
       "Content-type": "application/x-www-form-urlencoded; charset=UTF-8",
     },
-  }).then((x) => x.json());
+  })
+    .then((x) => x.json())
+    .then((x) => parseRdf(x.results.bindings));
   return resp;
 };
 
